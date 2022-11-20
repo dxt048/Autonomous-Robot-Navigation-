e1cc56e67d6bb52270c43c9280ca08c2bd57b773
dxt048 <91029666+dxt048@users.noreply.github.com>
2022-11-20 15:05:41 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 #include "mbed.h"
-#include <map>
 
 BusOut display1(PC_4, PB_13, PB_14, PB_15, PB_1, PB_2, PB_12, PA_11);
-BusOut display2(PC_3, PC_2, PB_7, PA_15, PA_14, PA_13, PC_12, PC_10);
+BusOut display2(PC_3, PC_2, PB_7, PA_14, PC_12, PC_10, PD_2, PC_11);
 
-DigitalOut in1(D5);
-DigitalOut in2(D4);
-DigitalOut in3(D3);
-DigitalOut in4(D2);
+BusOut motorControl(D2, D3, D4, D5);
 
 PwmOut ENA(D8);
 PwmOut ENB(D13);
 
 //Timers and Threads
 Thread displayingCoords;
-Timer controlTime;
+Thread updateCoords;
+Timer timerController;
+Timer displayUpdateTimer;
 
+//Cordinates and direction
 int x;
 int y;
+int direction;
+
+//Global variables
+bool inMotion; //Tracks if robot is currently in the middle of moving
+bool isTurning; //Tracks if robot is currently rotating
+int motionSteps; //Tracks what step into its movement the robot is in (This is just for hardcoded movements)
 
 //Return hex value to display on seven segment
 int displayHexValue(int number){
@@ -53,58 +58,75 @@ int displayHexValue(int number){
         case 9: 
             display = 0x6F;
             break;
+        default:
+            display = 0x79;
+            break;
     }
     return display;
 }
 
 //Display coordinates on both 7-segment displays
 void displaySegments(){
-    display1 = displayHexValue(x);
-    display2 = displayHexValue(y);
+    while(true){
+        display1 = displayHexValue(y);
+        display2 = displayHexValue(x);
+    }
 }
 
-//Functions to move robot
-void stop(){
-    in1 = 0;
-    in2 = 0;
-    in3 = 0;
-    in4 = 0;
+//Update x,y coordinates depending on direction in motion
+void updateCoordinates(){
+    switch(direction){
+        case 1:
+            y = y + 1;
+            break;
+        case 2:
+            x = x + 1;
+            break;
+        case 3:
+            y-=1;
+            break;
+        case 4:
+            x-=1;
+            break;
+    }
 }
 
-void forward(){
-    ENA=0.5;
-    ENB=0.5;
-    in1 = 1;
-    in2 = 0;
-    in3 = 1;        
-    in4 = 0;
+//Stops any robot movement and changes global bool variable to be false
+void robotStopMovement() {
+    motorControl = 0x00;
+    inMotion = false;
+    isTurning = false;
 }
 
-void reverse(){
-    ENA=0.5;
-    ENB=0.5;
-    in1 = 0;
-    in2 = 1;
-    in3 = 0;        
-    in4 = 1;
-}
-
-void left(){
-    ENA=0.5;
-    ENB=0.5;
-    in1 = 1;
-    in2 = 0;
-    in3 = 0;
-    in4 = 1;
-}
-
-void right(){
-    ENA=0.5;
-    ENB=0.5;
-    in1 = 0;
-    in2 = 1;
-    in3 = 1;
-    in4 = 0;
+//Main robot movement function
+void robotMotorMovements(int motorMovement){
+    switch(motorMovement){
+        case 1: //Stop 
+            motorControl = 0x00;
+            break;
+        case 2: //Left
+            motorControl = 0x09;
+            if(direction == 1) {
+                direction = 4;
+                break;
+            }
+            direction-=1;
+            break;
+        case 3: //Right
+            motorControl = 0x06;
+            if(direction == 4) {
+                direction = 1;
+                break;
+            }
+            direction+=1;
+            break;
+        case 4: //Forward
+            motorControl = 0x0A;
+            break;
+        case 5: //Reverse
+            motorControl = 0x05;
+            break;
+    }
 }
 
 // main() runs in its own thread in the OS
@@ -117,29 +139,88 @@ int main()
     ENB.period(0.05);
     ENB=0.5;
 
+    //Movement time at 50% duty cycle for 8 inches + 90 degree-ish turn
     double movementTime = 0.8;
-    double spinTime = 0.56;
+    double turnTime = 0.56;
 
-    controlTime.start();
-    forward();
-
+    //Initialize coords
     x = 1;
     y = 1;
+    direction = 1;
 
+    //Initialize global variables
+    inMotion = false;
+    isTurning = false;
+    motionSteps = 1;
+
+    //Start displaying segments continuously
     displayingCoords.start(displaySegments);
-    
 
+    //Initialize bool to end while loop
+    bool endOfProgram = false;
+
+    timerController.start();
+    //While loop
     while (true) {
-        //Turn stop moving forward and turn left after 4 seconds
-        if(controlTime.read() > 0.8){
-            stop();
-            controlTime.stop();
+        //Check if enough time has pass to update coordinates
+        if(inMotion && displayUpdateTimer.read() > movementTime/2){
+            updateCoordinates();
+            displayUpdateTimer.reset();
+            displayUpdateTimer.stop();
         }
-        //After 2 seconds of turning left, stop moving
-        if(controlTime.read() > 2){
-            stop();
-            controlTime.stop();
+        //Check if enough time to have moved 8 inches has passed
+        if(inMotion && timerController.read() > movementTime){
+            robotStopMovement();
+        }
+        //Check if enough time to turn 90 degrees has passed
+        if(isTurning && timerController.read() > turnTime){
+            robotStopMovement();
+        }
+
+        //If robot is not in motion or is not turning, check switch statement for next set of instructions
+        if (!inMotion && !isTurning) {
+            switch(motionSteps) {
+                case 1: //First step, move forward from (1,1) to (1,2)
+                    timerController.reset();
+                    displayUpdateTimer.start();
+                    robotMotorMovements(4);
+                    inMotion = true;
+                    motionSteps+=1;
+                    break;
+                case 2: //Second step, turn from north to east <direction goes from 1 -> 2>
+                    timerController.reset();
+                    robotMotorMovements(3);
+                    inMotion = true;
+                    motionSteps+=1;
+                    break;
+                case 3: //Third step, move forward from (1,2) to (2,2)
+                    timerController.reset();
+                    displayUpdateTimer.start();
+                    robotMotorMovements(4);
+                    inMotion = true;
+                    motionSteps+=1;
+                    break;
+                case 4: //Fourth step, turn from east to south <direction goes from 2 -> 3>
+                    timerController.reset();
+                    robotMotorMovements(3);
+                    inMotion = true;
+                    motionSteps+=1;
+                    break;
+                case 5: //Fifth step, move forward from (2,2) to (2,1)
+                    timerController.reset();
+                    displayUpdateTimer.start();
+                    robotMotorMovements(4);
+                    inMotion = true;
+                    motionSteps+=1;
+                    break;
+                case 26: //End of hardcoded path, exit loop
+                    endOfProgram = true;
+                    break;
+            }
+        }
+
+        if(endOfProgram){
+            break;
         }
     }
 }
-
